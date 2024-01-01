d7a1a075488f1323bfd115190b29c810d5dbff59
Nicolas Soler <jnsoler04@gmail.com>
2024-01-01 14:39:28 -0500

    Updates

---
+++ src/UI/Components/index.js
@@ -0,0 +1,11 @@
+import { default as CartItem } from './CartItem'
+import { default as Footer } from './Footer'
+import { default as Header } from './Header'
+import { default as Hero } from './Hero'
+import { default as Product } from './Product'
+import { default as Sidebar } from './Sidebar'
+import { default as InteractiveRouterLink } from './InteractiveRouterLink'
+
+export {
+    CartItem, Footer, Header, Hero, Product, Sidebar, InteractiveRouterLink
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './Infrastructure/App';
 
 ReactDOM.createRoot(
   document.getElementById("root"),
 ).render(
-  <div>
-    asdhjgasdjkhasgdhjksad
-    <h3 className="md:min-w-full text-blue-600 text-xs uppercase font-bold block pt-1 pb-4 no-underline">
-      Admin Layout Pages
-    </h3>
-    <h1 className="text-4xl text-blue-700">My Webpack + Tailwind App</h1>
-  </div>
+  <App />
 );

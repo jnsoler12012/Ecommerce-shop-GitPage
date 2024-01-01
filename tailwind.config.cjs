83755da3511ad4bfda805e3c55bfb4f2c5b3cd82
Nicolas Soler <jnsoler04@gmail.com>
2024-01-01 14:37:37 -0500

    gh page router

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,28 @@ module.exports = {
     mode: 'jit',
     content: ['./src/**/*.{js,jsx}', './dist/index.html'],
     theme: {
+        fontFamily: {
+            primary: 'Poppins',
+        },
+        container: {
+            padding: {
+                DEFAULT: '30px',
+                lg: '0',
+            },
+        },
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1440px',
+        },
         extend: {
             colors: {
-                primary: '#1B73E8',
+                primary: '#222222',
+                secondary: '#F5E6E0',
+            },
+            backgroundImage: {
+                hero: "url('./img/bg_hero.svg')",
             },
         },
     },

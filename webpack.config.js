615216a915622a669f6b45dce7edf48446c1fe81
Nicolas Soler <jnsoler04@gmail.com>
2024-01-01 12:09:28 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: `${__dirname}/src/index.js`,
+    output: {
+        path: `${__dirname}/dist`,
+        clean: true,
+        //publicPath: '/dist/',
+        publicPath: '/dist/',
+        filename: 'bundle.js',
+    },
+    historyApiFallback: {
+        rewrites: [{ from: /\//, to: '/404.html' }],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    devtool: process.argv.indexOf('-p') === -1 ? 'eval-source-map' : 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                include: [path.resolve(__dirname, 'src')],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    },
+
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+        ],
+    },
+    optimization:
+        process.argv.indexOf('-p') === -1
+            ? {}
+            : {
+                minimize: true,
+                minimizer: [
+                    new TerserPlugin({
+                        terserOptions: {
+                            output: {
+                                comments: false,
+                            },
+                        },
+                        extractComments: false,
+                    }),
+                ],
+            },
+}

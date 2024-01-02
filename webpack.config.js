07fc37c1de35c94cc8601d636ba677928357ad36
Nicolas Soler <jnsoler04@gmail.com>
2024-01-01 23:55:37 -0500

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         //publicPath: '/dist/',
         publicPath: '/dist/',
         filename: 'bundle.js',
+        assetModuleFilename: 'Ecommerce-shop-GitPage/[hash][ext][query]'
     },
     devServer: {
         historyApiFallback: {
